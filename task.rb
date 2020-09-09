0938449e714094dcd13decdbd1779531d1329fa5
shimabukurosusumugo <soccer_rough@yahoo.co.jp>
2020-09-09 21:07:02 +0900

    q1とq4修正、q6~q10回答

--- task.rb
+++ task.rb
@@ -7,7 +7,7 @@ def q1
   # 以下に回答を記載
 
   # "斎藤"を追加
-  p names.push("斎藤")
+  p names << "斎藤"
 
 end
 
@@ -33,7 +33,7 @@ def q4
   sports = ["サッカー", "フットサル", nil, "野球", "バスケ", nil, "バレー"]
 
   # 以下に回答を記載
-  sports.delete(nil)
+  sports.compact!
   # 以下は変更しないで下さい
   p sports
 end
@@ -52,6 +52,8 @@ def q6
   numbers1 = [1, 2, 3, 4, 5]
 
   # 以下に回答を記載
+  numbers2 = numbers1.map {|n|n*10}
+  puts numbers2
 
 end
 
@@ -59,6 +61,7 @@ def q7
   array = ["1", "2", "3", "4", "5"]
 
   # 以下に回答を記載
+  array.map!(&:to_i)
 
   # 以下は変更しないで下さい
   p array
@@ -68,7 +71,8 @@ def q8
   programming_languages = %w(ruby php python javascript)
 
   # 以下に回答を記載
-
+  programming_languages.map!(&:capitalize)
+  upper_case_programming_languages = programming_languages.map(&:upcase)
   # 以下は変更しないで下さい
   p programming_languages
   p upper_case_programming_languages
@@ -78,6 +82,9 @@ def q9
   names = ["田中", "佐藤", "佐々木", "高橋"]
 
   # 以下に回答を記載
+  names.each.with_index(1) do |name ,i|
+   puts "会員 No.#{i} #{name}さん"
+  end
 
 end
 
@@ -85,7 +92,9 @@ def q10
   foods = %w(いか たこ うに しゃけ うにぎり うに軍艦 うに丼)
 
   # 以下に回答を記載
-
+  foods.each do |food|
+    puts food.include?("うに")? "好物です" : "まあまあ好きです"
+  end
 end
 
 def q11
