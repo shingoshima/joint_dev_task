3be358d9577357ebeb393d8b10dac2a3a5e143dc
shimabukurosusumugo <soccer_rough@yahoo.co.jp>
2020-09-23 15:00:02 +0900

    rubyQ11~Q16を回答

--- task.rb
+++ task.rb
@@ -101,6 +101,10 @@ def q11
   sports = ["サッカー", "バスケ", "野球", ["フットサル", "野球"], "水泳", "ハンドボール", ["卓球", "サッカー", "ボルダリング"]]
 
   # 以下に回答を記載
+  puts "ユーザーの趣味一覧"
+  sports.flatten.uniq.each.with_index(1) do |sport, i|
+    puts "No#{i} #{sport}"
+  end
 
 end
 
@@ -108,6 +112,8 @@ def q12
   data = { user: { name: "satou", age: 33 } }
 
   # 以下に回答を記載
+  # dataからnameを出力
+  p data[:user][:name]
 
 end
 
@@ -116,14 +122,14 @@ def q13
   update_data = { age: 32, address: "沖縄" }
 
   # 以下に回答を記載
-
+  puts user_data.merge!(update_data)
 end
 
 def q14
   data = { name: "satou", age: 33, address: "saitama", hobby: "soccer", email: "hoge@fuga.com" }
 
   # 以下に回答を記載
-
+  p data.keys
 end
 
 def q15
@@ -131,7 +137,10 @@ def q15
   data2 = { name: "yamada", hobby: "baseball", role: "normal" }
 
   # 以下に回答を記載
-
+  data3 = [data1, data2]
+  data3.each do |data|
+    puts data[:age] ? "OK" : "NG"
+  end
 end
 
 def q16
@@ -143,6 +152,9 @@ def q16
   ]
 
   # 以下に回答を記載
+  users.each do |user|
+    puts "私の名前は#{user[:name]}です。年齢は#{user[:age]}です。"
+  end
 
 end
 
